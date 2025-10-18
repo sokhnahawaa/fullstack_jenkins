fce888141559028ecaad800ba782fb32b717027c
sokhnahawaa <157130515+sokhnahawaa@users.noreply.github.com>
2025-10-18 23:24:51 +0200

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,      // écoute sur 0.0.0.0
-    port: 5173,      // correspond au port exposé
-    strictPort: true // échoue si le port est déjà pris
+    host: '0.0.0.0',  // Écoute sur toutes les interfaces
+    port: 5173,
+    strictPort: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
   }
-});
+})
