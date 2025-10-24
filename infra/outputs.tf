077d647b6777d489522adbb8d9dc08c18785cd7f
sokhnahawaa <157130515+sokhnahawaa@users.noreply.github.com>
2025-10-24 14:55:10 +0200

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,9 @@
+output "ec2_public_ip" {
+  description = "Adresse IP publique de l’instance EC2"
+  value       = aws_instance.web.public_ip
+}
 
+output "rds_endpoint" {
+  description = "Endpoint de la base de données RDS"
+  value       = aws_db_instance.db.endpoint
+}
